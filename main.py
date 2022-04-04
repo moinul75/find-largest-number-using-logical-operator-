cb436ac448cbb481d3204c6539b97fd1c8bad133
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 15:37:47 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Using logical Opertor
+num1 = 20
+num2 = 10
+num3 = 30
+
+if num1>num2 and num1> num3:
+    print("The largest Number is: ", num1)
+elif num2>num1 and num2>num3:
+    print("The largest Number is: ", num2)
+else:
+    print("The largest number is: ", num3)
